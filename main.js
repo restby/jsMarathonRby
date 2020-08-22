b8feab272489e807607a9fcfc02edd6f69ee98e9
restby <restby@gmail.com>
2020-08-22 19:53:14 +0300

    homework-3

--- main.js
+++ main.js
@@ -1,58 +1,60 @@
-'use strict';
-//1
-const firstRow = prompt('напишите первую фразу');
-const secondRow = prompt('напишите вторую фразу');
-const CHAR = prompt('напишите букву');
-
-function getRow(firstRow, secondRow) {
-	let firstResult = findSymbol(firstRow, CHAR);
-	let secondResult = findSymbol(secondRow, CHAR);
-
-	if (firstResult === secondResult) {
-		return 'в этих фразая: одинаковое кол-во букв ' + '"' + CHAR + '"';
-	}
-
-	return firstRow > secondRow ? firstRow : secondRow;
+const button = document.getElementById('btn-kick');
+const character = {
+  name: 'Pikachu',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-character'),
+  elProgressbar: document.getElementById('progressbar-character'),
 }
 
-function findSymbol(row, char) {
-	let count = 0;
-
-	for (let i = 0; i < row.length; i++) {
-		if (row.charAt(i) === char) {
-			count++
-		}
-	}
-
-	return count;
+const enemy = {
+  name: 'Charmander',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-enemy'),
+  elProgressbar: document.getElementById('progressbar-enemy'),
 }
 
-console.log('в этой фразе: \"' + getRow(firstRow, secondRow) + '\" больше букв ' + '"' + CHAR + '"');
+button.addEventListener('click', function () {
+  console.log('kick');
+  changeHP(random(20), character);
+  changeHP(random(20), enemy);
+});
 
-alert('в этой фразе: \"' + getRow(firstRow, secondRow) + '\" больше букв ' + '"' + CHAR + '"');
-
-//2
-const phone = prompt('напишите номер телефона');
-
-function formattedPhone(phone) {
-	let phoneArray = phone.split('');
-
-	for (let i = 0; i < phoneArray.length; i++) {
-		if (i === 2) {
-			phoneArray.splice(i, '', ' (');
-		} else if (i === 6) {
-			phoneArray.splice(i, '', ') ');
-		} else if (i === 10) {
-			phoneArray.splice(i, '', '-');
-		} else if (i === 13) {
-			phoneArray.splice(i, '', '-');
-		}
-	}
-
-	let result = phoneArray.join('');
-	return result;
+function random(num) {
+  return Math.ceil(Math.random() * num);
 }
 
-console.log('отформатированный номер телефона теперь выглядит вот так: \"' + formattedPhone(phone));
+function renderHPLife(person) {
+  person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
+}
 
-alert('отформатированный номер телефона теперь выглядит вот так: \"' + formattedPhone(phone));
+function renderProgressbarHP(person) {
+  person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function renderHP(person) {
+  renderHPLife(person);
+  renderProgressbarHP(person);
+}
+
+function changeHP(count, person) {
+  if (person.damageHP < count) {
+    person.damageHP = 0
+    console.log('персонаж проиграл');
+    button.disabled = true;
+
+  } else {
+    person.damageHP -= count;
+  }
+
+  renderHP(person);
+}
+
+function init() {
+  console.log('Start Game!');
+  renderHP(character);
+  renderHP(enemy);
+}
+
+init();
